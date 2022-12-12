40dccce384d709f1bcf0f9e89bcf81f5273897e7
Sorathia13 <75586124+Sorathia13@users.noreply.github.com>
2022-12-12 16:00:15 +0100

    changeimage

--- js/test.js
+++ js/test.js
@@ -1,27 +1,33 @@
-var input = document.getElementById('username_field');
-var button = document.getElementById('validation_Username');
-
-input.addEventListener('input', function() {
-    if (this.value.length >= 5 ) {
-        button.classList.add('show');
-    } 
-    else {
-        button.classList.remove('show');
-    }
-    });
-    
-    function longueur(chaine) {
-        return chaine.length;
-    }
-
 /*change src of the image on click*/
 
-function changeImage() {
-    var logImage = document.getElementById('logImg');
-    if (logImage.src = "log1.png") {
-        logImage.src = "log2.png";
-    } else {
-        logImage.src = "log1.png";
-    }
+if (currentWebpage.match("index.html") || currentWebpage.match(/\/$/)) {
+    var input = document.getElementById('username_field');
+    var button = document.getElementById('validation_Username');
+
+    input.addEventListener('input', function () {
+        if (this.value.length >= 5) {
+            button.classList.add('show');
+        }
+        else {
+            button.classList.remove('show');
+        }
+    });
 }
 
+var logImage = document.getElementById('logImg');
+    function changeImage() {
+        if (logImage.src.match("img/log1.png")) {
+            logImage.src = "img/log2.png";
+            setTimeout(changeImage, 100);
+            console.log("image changed");
+        } else {
+            logImage.src = "img/log1.png";
+            console.log("image reinitialised");
+        }
+    }
+    
+
+
+if (currentWebpage.match("game.html") || currentWebpage.match(/\/$/)) {
+    changeImage();
+}
